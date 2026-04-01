bc3b6da8f4a58305056da76c7e93623ff58b157c
majda250 <bendifimajda0@gmail.com>
2026-04-01 14:39:37 +0100

    style(affichage): ajout couleurs avec variables
    Avant: codes couleur \033[0;32m directement dans les echo
    Après: variables VERT, ROUGE, JAUNE, BLEU, NC
    
    Avantages:
    - Code plus lisible
    - Plus facile à modifier les couleurs
    - Réduction des erreurs de syntaxe

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+ORANGE="\033[0;33m"
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+BLUE="\033[0;34m"
+NC="\033[0;0m"
+
 set -e 
 trap "echo erreur détectée" ERR  
-echo -e  "\e[33m===== Surveillance du système =====\e[0m"
-echo -e "\e[32mprocess status:\e[0m"
+echo -e  "${ORANGE}===== Surveillance du système =====${NC}"
+echo -e "${GREEN}process status:${NC}"
 ps
-echo -e "\e[32métat du disque:\e[0m"
+echo -e "${GREEN}état du disque:${NC}"
  df -h
-echo -e "\e[32mmémoire libre:\e[0m"
+echo -e "${GREEN}mémoire libre:${NC}"
 free -h
-echo -e "\e[32mtemps de fonctionnement du système:\e[0m"
+echo -e "${GREEN}temps de fonctionnement du système:${NC}"
 uptime
